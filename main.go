2c6d120df255e54ee6f25c90bdfc4771178b8509
Will Nilges <wdn5796@rit.edu>
2023-10-16 02:08:40 -0400

    Tune reactions, prioritize bot reactions
    And a lot more formatting and whatever

--- main.go
+++ main.go
@@ -92,7 +92,7 @@ func init() {
 
 	slackAPI = slack.New(config.SlackAccessToken)
 
-	statusHistory, err = getStatusHistory()
+	statusHistory, err = getChannelHistory()
 	if err != nil {
 		log.Println(err)
 	}
@@ -144,53 +144,56 @@ func statusPage(c *gin.Context) {
 	var currentStatus StatusUpdate
 	for _, message := range statusHistory {
 		teamID := fmt.Sprintf("<@%s>", config.SlackBotID)
-		if strings.Contains(message.Text, teamID) {
-			msgUser, err := slackAPI.GetUserInfo(message.User)
-			if err != nil {
-				fmt.Println(err)
-				c.String(http.StatusInternalServerError, "error reading request body: %s", err.Error())
-			}
-			realName := msgUser.RealName
-			var update StatusUpdate
-			update.Text = strings.Replace(message.Text, teamID, "", -1)
-			update.SentBy = realName
-			update.TimeStamp = slackTSToHumanTime(message.Timestamp)
-			update.Background = config.StatusNeutralColor
+		// Ignore messages that don't mention us. Also, ignore messages that
+		// mention us but are empty!
+		if !strings.Contains(message.Text, teamID) || message.Text == teamID {
+			continue
+		}
+		msgUser, err := slackAPI.GetUserInfo(message.User)
+		if err != nil {
+			fmt.Println(err)
+			c.String(http.StatusInternalServerError, "error reading request body: %s", err.Error())
+		}
+		realName := msgUser.RealName
+		var update StatusUpdate
+		update.Text = strings.Replace(message.Text, teamID, "", -1)
+		update.SentBy = realName
+		update.TimeStamp = slackTSToHumanTime(message.Timestamp)
+		update.Background = config.StatusNeutralColor
 
-			willBeCurrentStatus := false
-			shouldPin := false
-			for _, reaction := range message.Reactions {
-				// Only take action on our reactions
-				if botReaction := stringInSlice(reaction.Users, config.SlackBotID); !botReaction {
-					continue
-				}
-				// If we find a pin at all, then use it
-				if reaction.Name == config.CurrentEmoji && hasCurrentStatus == false {
-					willBeCurrentStatus = true
-				} else if reaction.Name == config.PinEmoji {
-					shouldPin = true
-				}
+		willBeCurrentStatus := false
+		shouldPin := false
+		for _, reaction := range message.Reactions {
+			// Only take action on our reactions
+			if botReaction := stringInSlice(reaction.Users, config.SlackBotID); !botReaction {
+				continue
+			}
+			// If we find a pin at all, then use it
+			if reaction.Name == config.CurrentEmoji && hasCurrentStatus == false {
+				willBeCurrentStatus = true
+			} else if reaction.Name == config.PinEmoji {
+				shouldPin = true
+			}
 
-				// Use the first reaction sent by the bot that we find
-				if update.Background == config.StatusNeutralColor {
-					switch reaction.Name {
-					case config.StatusOKEmoji:
-						update.Background = config.StatusOKColor
-					case config.StatusWarnEmoji:
-						update.Background = config.StatusWarnColor
-					case config.StatusErrorEmoji:
-						update.Background = config.StatusErrorColor
-					}
+			// Use the first reaction sent by the bot that we find
+			if update.Background == config.StatusNeutralColor {
+				switch reaction.Name {
+				case config.StatusOKEmoji:
+					update.Background = config.StatusOKColor
+				case config.StatusWarnEmoji:
+					update.Background = config.StatusWarnColor
+				case config.StatusErrorEmoji:
+					update.Background = config.StatusErrorColor
 				}
 			}
-			if willBeCurrentStatus {
-				currentStatus = update
-				hasCurrentStatus = true
-			} else if shouldPin && len(pinnedUpdates) < config.PinLimit {
-				pinnedUpdates = append(pinnedUpdates, update)
-			} else {
-				updates = append(updates, update)
-			}
+		}
+		if willBeCurrentStatus {
+			currentStatus = update
+			hasCurrentStatus = true
+		} else if shouldPin && len(pinnedUpdates) < config.PinLimit {
+			pinnedUpdates = append(pinnedUpdates, update)
+		} else {
+			updates = append(updates, update)
 		}
 	}
 
