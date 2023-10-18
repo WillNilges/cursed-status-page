adef814ab0eb64edaca46dea2bde8b8d6f8dc5ce
Will Nilges <wdn5796@rit.edu>
2023-10-17 20:42:49 -0400

    Rip all the crap out of page.go

--- page.go
+++ page.go
@@ -8,58 +8,9 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/slack-go/slack"
 )
 
-func generateSites(message slack.Message) (sites []Site, err error) {
-	channelID := config.SlackStatusChannelID
-	threadTs := message.Timestamp
-	// Get the conversation history
-	params := slack.GetConversationRepliesParameters{
-		ChannelID: channelID,
-		Timestamp: threadTs,
-	}
-	var siteMessages []slack.Message
-	siteMessages, _, _, err = slackAPI.GetConversationReplies(&params)
-	if err != nil {
-		return sites, err
-	}
-	
-	for _, message := range siteMessages {
-
-		botID := fmt.Sprintf("<@%s>", config.SlackBotID)
-		if strings.Contains(message.Text, botID) {
-			continue
-		}
-		var site Site
-		site.Name = message.Text
-		site.Background = config.StatusNeutralColor
-		for _, reaction := range message.Reactions {
-			// Only take action on our reactions
-			if botReaction := stringInSlice(reaction.Users, config.SlackBotID); !botReaction {
-				continue
-			}
-
-			// Use the first reaction sent by the bot that we find
-			switch reaction.Name {
-			case config.StatusOKEmoji:
-				site.Background = config.StatusOKColor
-				break
-			case config.StatusWarnEmoji:
-				site.Background = config.StatusWarnColor
-				break
-			case config.StatusErrorEmoji:
-				site.Background = config.StatusErrorColor
-				break
-			}
-		}
-		sites = append(sites, site)
-	}
-
-	return sites, nil
-}
-
-func buildStatusPage() (sites []Site, updates []StatusUpdate, pinnedUpdates []StatusUpdate, err error) {
+func buildStatusPage() (updates []StatusUpdate, pinnedUpdates []StatusUpdate, err error) {
 	log.Println("Building Status Page...")
 	for _, message := range globalChannelHistory {
 		botID := fmt.Sprintf("<@%s>", config.SlackBotID)
@@ -68,25 +19,11 @@ func buildStatusPage() (sites []Site, updates []StatusUpdate, pinnedUpdates []St
 		if !strings.Contains(message.Text, botID) || message.Text == botID {
 			continue
 		}
-		// Check if there's a sites reaction
-		for _, reaction := range message.Reactions {
-			if reaction.Name == config.SiteEmoji {
-				sites, err = generateSites(message)
-				if err != nil {
-					log.Println(err)
-					return sites, updates, pinnedUpdates, err
-				}
-				break
-			}
-		}
-		// Now that we've checked for the sites reaction, bail if we see "Sites"
-		if strings.Contains(message.Text, fmt.Sprintf("%s Sites", botID)) {
-			continue
-		}
+
 		msgUser, err := slackAPI.GetUserInfo(message.User)
 		if err != nil {
 			log.Println(err)
-			return sites, updates, pinnedUpdates, err
+			return updates, pinnedUpdates, err
 		}
 		realName := msgUser.RealName
 		var update StatusUpdate
@@ -95,15 +32,10 @@ func buildStatusPage() (sites []Site, updates []StatusUpdate, pinnedUpdates []St
 		update.TimeStamp = slackTSToHumanTime(message.Timestamp)
 		update.Background = config.StatusNeutralColor
 
-		shouldPin := false
 		for _, reaction := range message.Reactions {
 			// Only take action on our reactions
 			if botReaction := stringInSlice(reaction.Users, config.SlackBotID); !botReaction {
 				continue
-			}
-			// If we find a pin at all, then use it
-			if reaction.Name == config.PinEmoji {
-				shouldPin = true
 			}
 
 			// Use the first reaction sent by the bot that we find
@@ -118,14 +50,10 @@ func buildStatusPage() (sites []Site, updates []StatusUpdate, pinnedUpdates []St
 				}
 			}
 		}
-		if shouldPin && len(pinnedUpdates) < config.PinLimit {
-			pinnedUpdates = append(pinnedUpdates, update)
-		} else {
-			updates = append(updates, update)
-		}
+		updates = append(updates, update)
 	}
 
-	return sites, updates, pinnedUpdates, nil
+	return updates, pinnedUpdates, nil
 }
 
 func statusPage(c *gin.Context) {
@@ -136,7 +64,6 @@ func statusPage(c *gin.Context) {
 			"HelpMessage":    template.HTML(config.HelpMessage),
 			"PinnedStatuses": globalPinnedUpdates,
 			"StatusUpdates":  globalUpdates,
-			"Sites": globalSites, 
 			"Org":            config.OrgName,
 			"Logo":           config.LogoURL,
 			"Favicon":        config.FaviconURL,
