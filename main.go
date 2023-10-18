d52f8f326f8f3fb9a89dd119041e9700c18dfd43
Will Nilges <wdn5796@rit.edu>
2023-10-17 20:36:59 -0400

    Rip sites and stuff out of main

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"os"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -29,11 +28,6 @@ type Config struct {
 	StatusErrorColor   string
 	StatusErrorEmoji   string
 
-	PinEmoji string
-	PinLimit int
-
-	SiteEmoji string
-
 	NominalMessage string
 	NominalSentBy  string
 	HelpMessage    string
@@ -46,18 +40,12 @@ type StatusUpdate struct {
 	Background string
 }
 
-type Site struct {
-	Name string
-	Background string
-}
-
 var config Config
 
 var globalChannelHistory []slack.Message
 
 var globalUpdates []StatusUpdate
 var globalPinnedUpdates []StatusUpdate
-var globalSites []Site
 
 var slackAPI *slack.Client
 
@@ -85,12 +73,6 @@ func init() {
 	config.StatusErrorColor = os.Getenv("CSP_CARD_ERROR_COLOR")
 	config.StatusErrorEmoji = os.Getenv("CSP_CARD_ERROR_EMOJI")
 
-	config.PinEmoji = os.Getenv("CSP_PIN_EMOJI")
-	config.PinLimit, _ = strconv.Atoi(os.Getenv("CSP_PIN_LIMIT"))
-
-
-	config.SiteEmoji = os.Getenv("CSP_SITES_EMOJI")
-
 	config.NominalMessage = os.Getenv("CSP_NOMINAL_MESSAGE")
 	config.NominalSentBy = os.Getenv("CSP_NOMINAL_SENT_BY")
 	config.HelpMessage = os.Getenv("CSP_HELP_LINK")
@@ -108,7 +90,7 @@ func init() {
 	config.SlackBotID = authTestResponse.UserID
 
 	// Initialize the actual data we need for the status page
-	globalSites, globalUpdates, globalPinnedUpdates, err = buildStatusPage()
+	globalUpdates, globalPinnedUpdates, err = buildStatusPage()
 	if err != nil {
 		log.Fatal(err)
 	}
