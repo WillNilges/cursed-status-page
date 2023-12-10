dc7b1c8bfed3834b3ff7e4dc44c1902d68989f7c
Willard Nilges <wdn5796@rit.edu>
2023-12-10 14:43:02 -0500

    Make NN parsing work on uppercase NNs (#51)
    This makes nn713, OR NN713 work.

--- static/scripts/parse_nn.js
+++ static/scripts/parse_nn.js
@@ -1,15 +1,19 @@
 window.onload = function() {
     // Define a regular expression to match the "nn713" format
     var regex = /nn(\d+)/g;
+    var capRegex = /NN(\d+)/g;
 
     // Get the HTML content of the entire page
     var pageContent = document.body.innerHTML;
 
     // Replace all occurrences of "nn713" with the link
-    var replacedContent = pageContent.replace(regex, function(match, number) {
-        return '<a target="_blank" href="https://www.nycmesh.net/map/nodes/' + number + '">' + match + '</a>';
-    });
+    var replacedContent = pageContent.replace(regex, parseNode);
+    replacedContent = replacedContent.replace(capRegex, parseNode);
 
     // Update the HTML content of the page with the modified content
     document.body.innerHTML = replacedContent;
 };
+
+function parseNode(match, number) {
+    return '<a target="_blank" href="https://www.nycmesh.net/map/nodes/' + number + '">' + match + '</a>';
+}
